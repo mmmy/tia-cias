7cd1cd3240968ecfc3e45b2a48d852c573e15f8a
yangqi <531905408@qq.com>
2023-12-18 20:45:37 +0800

    up

--- cias.ts
+++ cias.ts
@@ -1,26 +1,30 @@
 import { readFile } from "fs/promises"
 import { DirectSecp256k1HdWallet, OfflineDirectSigner } from "@cosmjs/proto-signing"
 import { IndexedTx, SigningStargateClient, StargateClient } from "@cosmjs/stargate"
-import { MsgSend } from "cosmjs-types/cosmos/bank/v1beta1/tx"
-import { Tx } from "cosmjs-types/cosmos/tx/v1beta1/tx"
+// import { MsgSend } from "cosmjs-types/cosmos/bank/v1beta1/tx"
+// import { Tx } from "cosmjs-types/cosmos/tx/v1beta1/tx"
+import * as config from "config"
+
+const runConfig: { run_times: number; gas_amount: number; key_paths: string[] } = config.get("cias")
+console.log(runConfig)
 
 const rpc = "https://rpc-1.celestia.nodes.guru"
 
 //-------cias挖矿程序-----------
 // ---------------------配置--------------------------
-const KEY_PATH = "../temp/tempkeys"
-//958 1958
-const GAS_AMOUNT = 58
-// 循环运行次数
-const RUN_TIMES = 3
+// const KEY_PATH = "../temp/tempkeys"
+// //958 1958
+// const GAS_AMOUNT = 58
+// // 循环运行次数
+// const RUN_TIMES = 3
 
-const getAliceSignerFromMnemonic = async (): Promise<OfflineDirectSigner> => {
-    return DirectSecp256k1HdWallet.fromMnemonic((await readFile(KEY_PATH)).toString(), {
+const getAliceSignerFromMnemonic = async (keyPath: string): Promise<OfflineDirectSigner> => {
+    return DirectSecp256k1HdWallet.fromMnemonic((await readFile(keyPath)).toString(), {
         prefix: "celestia",
     })
 }
 
-const runAll = async (): Promise<void> => {
+const runAll = async (keyPath: string): Promise<void> => {
     const startTime = new Date()
     const client = await StargateClient.connect(rpc)
     // console.log("With client, chain id:", await client.getChainId(), ", height:", await client.getHeight())
@@ -29,7 +33,7 @@ const runAll = async (): Promise<void> => {
     //     await client.getAllBalances("celestia12rg66ggrjz8yaquhqlvnh2y5fpghr3sfs4dzl9")
     // )
 
-    const aliceSigner: OfflineDirectSigner = await getAliceSignerFromMnemonic()
+    const aliceSigner: OfflineDirectSigner = await getAliceSignerFromMnemonic(keyPath)
     const address = (await aliceSigner.getAccounts())[0].address
     console.log("address:", address)
     const signingClient = await SigningStargateClient.connectWithSigner(rpc, aliceSigner, {
@@ -51,7 +55,7 @@ const runAll = async (): Promise<void> => {
         [{ denom: "utia", amount: "1000" }],
         // "auto",
         {
-            amount: [{ denom: "utia", amount: GAS_AMOUNT + "" }],
+            amount: [{ denom: "utia", amount: runConfig.gas_amount + "" }],
             gas: "74110",
         },
         "ZGF0YToseyJvcCI6Im1pbnQiLCJhbXQiOjEwMDAwLCJ0aWNrIjoiY2lhcyIsInAiOiJjaWEtMjAifQ=="
@@ -70,17 +74,35 @@ const runAll = async (): Promise<void> => {
 //         }, 2000)
 //     })
 // }
-async function run_recursion() {
+async function run_recursion(keyPath: string) {
     // new Array(RUN_TIMES).fill(0).map(async (v, index) => {})
-    for (let i = 0; i < RUN_TIMES; i++) {
+    let successCount = 0
+    for (let i = 0; i < runConfig.run_times; i++) {
         console.log("---------------------------")
-        console.log("开始执行: ", `${i + 1}/${RUN_TIMES}`, "现在时间", new Date().toLocaleTimeString())
+        console.log(
+            "开始执行: ",
+            `${i + 1}/${runConfig.run_times}`,
+            `成功 ${successCount}次`,
+            keyPath,
+            "现在时间",
+            new Date().toLocaleTimeString()
+        )
         console.log("---------------------------")
-        await runAll()
+        try {
+            await runAll(keyPath)
+            successCount += 1
+        } catch (e) {
+            console.log(keyPath, `第${i + 1}次`, "出现错误", e)
+        }
     }
+    console.log(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
+    console.log(keyPath, "已经全部结束")
+    console.log(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
 }
 
-run_recursion()
+runConfig.key_paths.forEach((pathStr) => {
+    run_recursion(pathStr)
+})
 
 // let count = 0
 // let successCount = 0
